86ec27d180b9be3ed3dcd113d65db39ccbff7308
manuelgermanos <78905820+manuelgermanos@users.noreply.github.com>
2023-05-04 17:35:19 +0300

    recommendation of transitions
    the system starts at a url and detects to which url the user scanned next, then increments a value inside a mongodb collection which houses all the possible transitions

--- py/recommendation_test.py
+++ py/recommendation_test.py
@@ -1,7 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify , session
 from pymongo import MongoClient
 
+
+
 app = Flask(__name__)
+app.secret_key = 'seb5goat'
 
 # Define the MongoDB connection string
 connection_string = "mongodb+srv://admin:admin@coe416database.qlhbx66.mongodb.net/"
@@ -18,44 +21,40 @@ constructors_collection = db.constructors
 
 
 @app.route('/constructor/<constID>')
-def get_constructor(constID):
+def constructor(constID):
     # Select the database and collections to use
     db = client.f1_2
     constructors = db.constructors
     drivesFor = db.drivesFor
-    drivers = db.drivers
+    rec1 = db.rec1
 
-    # Retrieve the constructor with the given constID
-    constructor = constructors.find_one({'constID': constID}, {'_id': 0})
+    # Retrieve the details of the selected constructor
+    constructor_data = constructors.find_one({'constID': constID}, {'_id': 0})
+    if constructor_data is None:
+        return jsonify({'error': 'Invalid constID'}), 400
 
-    if constructor:
-        # Retrieve the drivers that drive for this constructor
-        driverIDs = [d['driverID'] for d in drivesFor.find({'constID': constID}, {'_id': 0})]
-        driver_docs = drivers.find({'driverID': {'$in': driverIDs}}, {'_id': 0})
-        drivers_data = list(driver_docs)
+    # Retrieve the drivers that drive for the selected constructor
+    drivers_data = list(drivesFor.find({'constID': constID}, {'_id': 0, 'driverID': 1}))
 
-        # Add the drivers data to the constructor dictionary
-        constructor['drivers'] = drivers_data
+    # Check if there is a previously selected constructor in the session
+    prev_constID = session.get('prev_constID')
 
-        # Return the data as JSON
-        return jsonify(constructor)
-    else:
-        # Return a 404 error if the constructor is not found
-        return jsonify({'error': 'Constructor not found'}), 404
+    # If there is a previously selected constructor, increment the corresponding record in the rec1 collection
+    if prev_constID is not None and prev_constID != constID:
+        trans = f"{prev_constID}xx{constID}"
+        rec1.update_one({'trans': trans}, {'$inc': {'num': 1}}, upsert=True)
 
+    # Store the current constructor in the session
+    session['prev_constID'] = constID
 
-@app.route('/')
-def index():
-    # Create links to the different endpoints
-    links = {
-        'all_drivers': '/drivers',
-        'all_constructors': '/constructors',
-        'constructor': '/constructor/<constID>'
+    # Combine the data into a single dictionary
+    data = {
+        'constructor': constructor_data,
+        'drivers': drivers_data,
     }
 
-    # Return the links as JSON
-    return jsonify(links)
-
+    # Return the data as JSON
+    return jsonify(data)
 
 
 if __name__ == '__main__':
