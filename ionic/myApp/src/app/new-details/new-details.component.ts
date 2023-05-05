dce3f07bcf63badd0c3a0407eedcec34351e8f2b
manuelgermanos <78905820+manuelgermanos@users.noreply.github.com>
2023-05-05 14:18:10 +0300

    ionic components

---
+++ ionic/myApp/src/app/new-details/new-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-details',
+  templateUrl: './new-details.component.html',
+  styleUrls: ['./new-details.component.scss'],
+})
+export class NewDetailsComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
